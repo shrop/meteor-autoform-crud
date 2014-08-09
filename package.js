cda2935c5b6b4698c1afca1b10de88f8966e07e3
Mark Shropshire <mdshrops@shropnet.net>
2014-08-09 10:10:13 -0400

    Added new package description info

--- package.js
+++ package.js
@@ -1,4 +1,9 @@
 Package.describe({
-  name: "autoform-crud",
   summary: "Easy to setup autoform-based CRUD forms for collections."
+});
+
+Package.on_use(function (api, where) {
+});
+
+Package.on_test(function (api) {
 });
